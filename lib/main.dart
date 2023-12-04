20d1c6f1cad570fdc71b30aa7b6a33d0f1632d71
shafin678 <shafin2000fdr@gmail.com>
2023-12-04 21:11:19 +0600

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:assingment_1/page1.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          color: Colors.green,
+          child: ElevatedButton(
+            child: Text("Press me",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder:(context){return page1();}));
+              
+            },
+          ),
+
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/page1.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class page1 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Page 2",style: TextStyle(fontSize: 30,color: Colors.greenAccent),),
+          backgroundColor: Colors.white30,
+        )
+    );
+  }
+}
